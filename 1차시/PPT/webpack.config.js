2b20e6716d5c5133eb1f9ef561d02bd533dc2ed6
bhbh1226 <bhbh1226@gmail.com>
2018-02-13 22:32:44 +0900

    change error

---
+++ 1차시/PPT/webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+
+    module: {
+            loaders: [
+                {
+                    test: /\.js$/,
+                    loader: 'babel-loader',
+                    exclude: /node_modules/,
+                    query: {
+                        cacheDirectory: true,
+                        presets: ['es2015', 'react']
+                    }
+                }
+            ]
+        }
+};
